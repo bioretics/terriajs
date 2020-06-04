78131a29d4df65f394e7d0a79c01b740fa7b5776
glughi <giovanni.lughi@gmail.com>
2020-06-04 18:39:25 +0200

    Improved Help menu.

--- lib/ReactViews/HelpScreens/HelpMenuPanelBasic.jsx
+++ lib/ReactViews/HelpScreens/HelpMenuPanelBasic.jsx
@@ -70,6 +70,53 @@ const HelpMenuPanelBasic = createReactClass({
               {t("helpMenu.helpMenuHeader")}
             </label>
             <ul className={Styles.viewerSelector}>
+            <li className={Styles.listItem}>
+                <a
+                  target="_blank"
+                  href="https://geoportale.regione.emilia-romagna.it/it/contenuti/geoportale-3d"
+                  className={Styles.btnViewer}
+                >
+                  About
+                </a>
+              </li>
+              <li className={Styles.listItem}>
+                <button
+                  onClick={() => {
+                    var message = `
+                      <div>
+                          <p>La navigazione della mappa 3D è possibile in uno dei seguenti modi:</p>
+                          <p>* usando il mouse ed il tasto CTRL;</p>
+                          <p>* tramite i controlli sulla destra dell'interfaccia;</p>
+                          <p>* attivando il bottone "Naviga da tastiera" ed usando poi questi tasti della tastiera:<br>
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; w = zoom in<br>
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; s = zoom out<br>
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; q = muovi in su<br>
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; e = muovi in giù<br>
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; d = muovi a sinistra<br>
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; a = muovi a destra<br>
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; r = ruota in su<br>
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; f = ruota in giù<br>
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; z = ruota a sinistra<br>
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; x = ruota a destra</p>
+                      </div>
+                    `;
+                    var options = {
+                      title: 'Come navigare',
+                      confirmText: ("Ok"),
+                      width: 600,
+                      height: 450,
+                      message: message,
+                      horizontalPadding : 100
+                    };
+                    this.props.viewState.notifications.push(options);
+                  }}
+                  className={Styles.btnViewer}
+                >
+              
+                  {/*<NavigationHelpButton terria={this.props.terria} viewState={this.props.viewState}/>*/}
+                  Come navigare
+                  </button>
+              </li>
               <li className={Styles.listItem}>
                 <button
                   onClick={() => {
