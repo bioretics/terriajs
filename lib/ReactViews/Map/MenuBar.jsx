2d807f4029767abc638db83b441dcbf00234276a
glughi <giovanni.lughi@gmail.com>
2020-12-22 16:14:33 +0100

    Working on better POI management.

--- lib/ReactViews/Map/MenuBar.jsx
+++ lib/ReactViews/Map/MenuBar.jsx
@@ -18,6 +18,25 @@ import CoordsConverterPanel from './Panels/CoordsConverterPanel.jsx';
 import ColorMapPanel from './Panels/ColorMapPanel.jsx';
 //import NavigationHelpButton from './Navigation/NavigationHelpButton.jsx';
 
+var Primitive = require("terriajs-cesium/Source/Scene/Primitive").default;
+var GroundPrimitive = require("terriajs-cesium/Source/Scene/GroundPrimitive").default;
+var PerInstanceColorAppearance = require("terriajs-cesium/Source/Scene/PerInstanceColorAppearance").default;
+var GeometryInstance = require("terriajs-cesium/Source/Core/GeometryInstance").default;
+var RectangleGeometry = require("terriajs-cesium/Source/Core/RectangleGeometry").default;
+var CircleGeometry = require("terriajs-cesium/Source/Core/CircleGeometry").default;
+var BoxGeometry = require("terriajs-cesium/Source/Core/BoxGeometry").default;
+var Color = require("terriajs-cesium/Source/Core/Color").default;
+var Rectangle = require("terriajs-cesium/Source/Core/Rectangle").default;
+var Cartesian3 = require("terriajs-cesium/Source/Core/Cartesian3").default;
+var Matrix4 = require("terriajs-cesium/Source/Core/Matrix4").default;
+var Transforms = require("terriajs-cesium/Source/Core/Transforms").default;
+var ColorGeometryInstanceAttribute = require("terriajs-cesium/Source/Core/ColorGeometryInstanceAttribute").default;
+
+
+var CesiumMath = require("terriajs-cesium/Source/Core/Math").default;
+var Cartographic = require("terriajs-cesium/Source/Core/Cartographic").default;
+
+
 // The map navigation region
 const MenuBar = createReactClass({
   displayName: "MenuBar",
@@ -42,12 +61,85 @@ const MenuBar = createReactClass({
     this.props.viewState.topElement = "MenuBar";
   },
 
+
+
+  ggg() {
+    var viewRect = new GeometryInstance({
+      geometry: new RectangleGeometry({
+        rectangle: this.props.terria.cesium.scene.camera.computeViewRectangle(
+          this.props.terria.cesium.scene.globe.ellipsoid
+        )
+      }),
+      id: 'viewRectangle',
+      attributes: {
+        color: new ColorGeometryInstanceAttribute(0.0, 0.0, 1.0, 0.2)
+      }
+    });
+    /* --- */
+    var origRect = this.props.terria.cesium.scene.camera.computeViewRectangle(this.props.terria.cesium.scene.globe.ellipsoid);
+    var cameraPos = this.props.terria.cesium.scene.camera.positionWC;
+    console.log(cameraPos);
+    var coeff = 10;
+    var altitude = this.props.terria.cesium.scene.camera.positionCartographic.height;
+    var pos1 = new Cartesian3(
+      cameraPos.x - altitude * coeff,
+      cameraPos.y - altitude * coeff,
+      cameraPos.z
+    );
+    var pos2 = new Cartesian3(
+      cameraPos.x + altitude * coeff,
+      cameraPos.y + altitude * coeff,
+      cameraPos.z
+    );
+    console.log(pos1);
+    console.log(pos2);
+    var newRect = Rectangle.fromCartesianArray([pos1, pos2], this.props.terria.cesium.scene.globe.ellipsoid);
+
+    console.log(newRect);
+
+    var tmpRectangle = new GeometryInstance({
+      geometry: new RectangleGeometry({
+        rectangle: newRect
+      }),
+      id: 'correctedRectangle',
+      attributes: {
+        color: new ColorGeometryInstanceAttribute(0.0, 1.0, 0.0, 0.6)
+      }
+    });
+    /* --- */
+    var cameraPos = this.props.terria.cesium.scene.camera.positionWC;
+    var cameraRect = new GeometryInstance({
+      geometry: new CircleGeometry({
+        center: cameraPos,
+        radius: 100
+      }),
+      id: 'cameraRectangle',
+      attributes: {
+        color: new ColorGeometryInstanceAttribute(1.0, 0.0, 0.0, 0.5)
+      }
+    });
+    /* --- */
+    this.props.terria.cesium.scene.primitives.add(new GroundPrimitive({
+      geometryInstances: [tmpRectangle]
+    }));
+  },
+
+
+  onCoordButtonClick() {
+    this.props.viewState.coordShown = !this.props.viewState.coordShown;
+    this.props.terria.currentViewer.notifyRepaintRequired();
+    // Allow any animations to finish, then trigger a resize.
+    setTimeout(function () {
+      triggerResize();
+    }, this.props.animationDuration || 1);
+    //this.props.viewState.toggleFeaturePrompt("coord", false, true);
+  },
   onStoryButtonClick() {
     this.props.viewState.storyBuilderShown = !this.props.viewState
       .storyBuilderShown;
     this.props.terria.currentViewer.notifyRepaintRequired();
     // Allow any animations to finish, then trigger a resize.
-    setTimeout(function() {
+    setTimeout(function () {
       triggerResize();
     }, this.props.animationDuration || 1);
     this.props.viewState.toggleFeaturePrompt("story", false, true);
@@ -77,16 +169,16 @@ const MenuBar = createReactClass({
           </div>
         </Trans>
       ) : (
-        <Trans i18nKey="story.promptHtml2">
-          <div>
-            <small>INTRODUCING</small>
-            <h3>Data Stories</h3>
+          <Trans i18nKey="story.promptHtml2">
             <div>
-              Create and share interactive stories directly from your map.
+              <small>INTRODUCING</small>
+              <h3>Data Stories</h3>
+              <div>
+                Create and share interactive stories directly from your map.
             </div>
-          </div>
-        </Trans>
-      );
+            </div>
+          </Trans>
+        );
     const delayTime =
       storyEnabled && this.props.terria.stories.length > 0 ? 1000 : 2000;
     return (
@@ -111,12 +203,17 @@ const MenuBar = createReactClass({
               viewState={this.props.viewState}
             />
           </li>
-          {/*<li className={Styles.menuItem}>
-            <CoordsConverterPanel terria={this.props.terria} viewState={this.props.viewState}/>
-          </li>*/}
           <li className={Styles.menuItem}>
-            <ColorMapPanel terria={this.props.terria} viewState={this.props.viewState}/>
+            <CoordsConverterPanel terria={this.props.terria} viewState={this.props.viewState}/>
           </li>
+          <li className={Styles.menuItem}>
+            <ColorMapPanel terria={this.props.terria} viewState={this.props.viewState} />
+          </li>
+          {/*<li className={Styles.menuItem}>
+            <button onClick={this.ggg}>
+              GGG
+            </button>
+          </li>*/}
           <If condition={storyEnabled}>
             <li className={Styles.menuItem}>
               <button
