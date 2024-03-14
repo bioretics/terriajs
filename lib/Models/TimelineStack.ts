26348909dc0266291794839036df31af7593fe9f
glughi <giovanni.lughi@gmail.com>
2024-03-14 15:22:19 +0100

    Fix Timeline that doesn’t restart after it has been closed once

--- lib/Models/TimelineStack.ts
+++ lib/Models/TimelineStack.ts
@@ -86,6 +86,7 @@ export default class TimelineStack {
     }
     if (this._disposeTickSubscription) {
       this._disposeTickSubscription();
+      this._disposeTickSubscription = undefined;
     }
   }
 
