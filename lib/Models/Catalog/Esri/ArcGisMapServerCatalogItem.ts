b5445a1c2bfcdcaded74767e2e8d7b93ff5db0e8
glughi <giovanni.lughi@gmail.com>
2024-05-30 22:18:59 +0200

    Shows all the elements of the legend of ArcGisMapServer regardless of the equality of their images

--- lib/Models/Catalog/Esri/ArcGisMapServerCatalogItem.ts
+++ lib/Models/Catalog/Esri/ArcGisMapServerCatalogItem.ts
@@ -313,7 +313,8 @@ class MapServerStratum extends LoadableStratum(
       });
     });
 
-    items = uniqWith(items, (a, b) => a.imageUrl === b.imageUrl);
+    // Comment, otherwise the legend may be truncated when the same symbol is used multiple times
+    //items = uniqWith(items, (a, b) => a.imageUrl === b.imageUrl);
 
     return [createStratumInstance(LegendTraits, { items })];
   }
