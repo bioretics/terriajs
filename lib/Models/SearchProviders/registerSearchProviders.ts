85741b2977d1d2feeee86f19e6e70b77afd7aa6c
glughi <giovanni.lughi@gmail.com>
2024-09-17 19:16:08 +0200

    Add RerSearchProvider

--- lib/Models/SearchProviders/registerSearchProviders.ts
+++ lib/Models/SearchProviders/registerSearchProviders.ts
@@ -1,12 +1,18 @@
 import AustralianGazetteerSearchProvider from "./AustralianGazetteerSearchProvider";
 import BingMapsSearchProvider from "./BingMapsSearchProvider";
 import CesiumIonSearchProvider from "./CesiumIonSearchProvider";
+import RerSearchProvider from "./RerSearchProvider";
 import SearchProviderFactory from "./SearchProviderFactory";
 
 export default function registerSearchProviders() {
   SearchProviderFactory.register(
     BingMapsSearchProvider.type,
     BingMapsSearchProvider
+  );
+
+  SearchProviderFactory.register(
+    RerSearchProvider.type,
+    RerSearchProvider
   );
 
   SearchProviderFactory.register(
