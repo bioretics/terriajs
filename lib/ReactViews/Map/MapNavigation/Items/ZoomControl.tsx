8f5efb2e7baa40e4bf31c184b26f2188d6ed9441
Francesco Pazzaglia <francesco.pazzaglia2@gmail.com>
2025-02-11 10:51:11 +0100

    Fixed typography error.

--- lib/ReactViews/Map/MapNavigation/Items/ZoomControl.tsx
+++ lib/ReactViews/Map/MapNavigation/Items/ZoomControl.tsx
@@ -196,7 +196,7 @@ class ZoomControlBase extends React.Component<PropTypes> {
               <RawButton
                 type="button"
                 onClick={this.zoomIn.bind(this)}
-                title={t("zoomCotrol.zoomIn")}
+                title={t("zoomControl.zoomIn")}
               >
                 <Icon glyph={Icon.GLYPHS.zoomIn} />
               </RawButton>
@@ -206,7 +206,7 @@ class ZoomControlBase extends React.Component<PropTypes> {
             <RawButton
               type="button"
               onClick={this.zoomReset.bind(this)}
-              title={t("zoomCotrol.zoomReset")}
+              title={t("zoomControl.zoomReset")}
             >
               <Icon glyph={Icon.GLYPHS.zoomReset} />
             </RawButton>
@@ -216,7 +216,7 @@ class ZoomControlBase extends React.Component<PropTypes> {
               <RawButton
                 type="button"
                 onClick={this.zoomOut.bind(this)}
-                title={t("zoomCotrol.zoomOut")}
+                title={t("zoomControl.zoomOut")}
               >
                 <Icon glyph={GLYPHS.zoomOut} />
               </RawButton>
