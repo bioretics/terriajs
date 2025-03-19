1605895fd4055933a02deabf003dccd57dcd5a23
Francesco Pazzaglia <francesco.pazzaglia2@gmail.com>
2025-03-19 11:56:50 +0100

    Fixed gulp lint errors.

--- lib/Models/Catalog/CatalogItems/KmlCatalogItem.ts
+++ lib/Models/Catalog/CatalogItems/KmlCatalogItem.ts
@@ -212,9 +212,9 @@ class KmlCatalogItem
     const polygons = entities.filter((e) => e?.polygon);
     const polylines = entities.filter((e) => e?.polyline);
 
-    if (polygons.length == 1) {
+    if (polygons.length === 1) {
       return this.isPolygonValid(polygons) || this.arePolylinesValid(polygons);
-    } else if (polylines.length == 1) {
+    } else if (polylines.length === 1) {
       return (
         this.isPolygonValid(polylines) || this.arePolylinesValid(polylines)
       );
