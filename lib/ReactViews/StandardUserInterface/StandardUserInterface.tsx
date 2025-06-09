378ad12fae1883039b6ca632ba12915a932819b3
lughi <giovanni.lughi@gmail.com>
2025-06-09 15:14:51 +0200

    Increases the screen width threshold for mobile mode.

--- lib/ReactViews/StandardUserInterface/StandardUserInterface.tsx
+++ lib/ReactViews/StandardUserInterface/StandardUserInterface.tsx
@@ -78,7 +78,7 @@ const StandardUserInterfaceBase: React.FC<StandardUserInterfaceProps> =
     };
 
     const shouldUseMobileInterface = () =>
-      document.body.clientWidth < (props.minimumLargeScreenWidth ?? 768);
+      document.body.clientWidth < (props.minimumLargeScreenWidth ?? 1100);
 
     const resizeListener = action(() => {
       props.viewState.useSmallScreenInterface = shouldUseMobileInterface();
