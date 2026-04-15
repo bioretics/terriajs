ab4cf8a9ab3fa04c6709c77a4376bc7630acec1b
Giovanni Lughi <giovanni.lughi@gmail.com>
2026-04-15 23:14:19 +0200

    Fix image height in RelatedMaps

--- lib/ReactViews/RelatedMaps/RelatedMaps.tsx
+++ lib/ReactViews/RelatedMaps/RelatedMaps.tsx
@@ -57,8 +57,8 @@ class RelatedMaps extends React.Component<PropTypes> {
                   style={{
                     marginRight: "10px",
                     marginBottom: "10px",
-                    width: "200px",
-                    height: "150px"
+                    width: "200px"
+                    //height: "150px"
                   }}
                   src={map.imageUrl}
                   alt={map.title}
