bf0215f7f796250371626ae6b7493a2d211712c5
jcvasi <jacopovasiwork@gmail.com>
2026-08-27 08:47:24 +0200

    Added GetCapabilitiesTraits dependency to CatalogGroupTrait for useAuthentication

--- lib/Traits/TraitsClasses/CatalogGroupTraits.ts
+++ lib/Traits/TraitsClasses/CatalogGroupTraits.ts
@@ -1,22 +1,11 @@
 import CatalogMemberTraits from "./CatalogMemberTraits";
+import GetCapabilitiesTraits from "./GetCapabilitiesTraits";
 import GroupTraits from "./GroupTraits";
 import mixTraits from "../mixTraits";
-import primitiveTrait from "../Decorators/primitiveTrait";
 
-/**
- * `allowedGroups` / `hideWhenUnauthorized` come from CatalogMemberTraits.
- * `useAuthentication` is declared here so plain `type: "group"` catalogues can
- * set it (OGC groups already get it via GetCapabilitiesTraits).
- */
+/** `allowedGroups` / `hideWhenUnauthorized` from CatalogMemberTraits; `useAuthentication` from GetCapabilitiesTraits. */
 export default class CatalogGroupTraits extends mixTraits(
   GroupTraits,
-  CatalogMemberTraits
-) {
-  @primitiveTrait({
-    type: "boolean",
-    name: "Use authentication",
-    description:
-      "If true, members may inherit use of the configured authentication method when they do not set `useAuthentication` themselves."
-  })
-  useAuthentication?: boolean;
-}
+  CatalogMemberTraits,
+  GetCapabilitiesTraits
+) {}
