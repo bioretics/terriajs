c4f5ca4f4375d5cd37efcc2a1487bbad79bb4fa9
jcvasi <jacopovasiwork@gmail.com>
2026-08-27 08:29:10 +0200

    Added catalog group protected traits handling to CatalogAccessControl and useAuthentication trait to CatalogGroupTraits

--- lib/Traits/TraitsClasses/CatalogGroupTraits.ts
+++ lib/Traits/TraitsClasses/CatalogGroupTraits.ts
@@ -1,8 +1,22 @@
 import CatalogMemberTraits from "./CatalogMemberTraits";
 import GroupTraits from "./GroupTraits";
 import mixTraits from "../mixTraits";
+import primitiveTrait from "../Decorators/primitiveTrait";
 
+/**
+ * `allowedGroups` / `hideWhenUnauthorized` come from CatalogMemberTraits.
+ * `useAuthentication` is declared here so plain `type: "group"` catalogues can
+ * set it (OGC groups already get it via GetCapabilitiesTraits).
+ */
 export default class CatalogGroupTraits extends mixTraits(
   GroupTraits,
   CatalogMemberTraits
-) {}
+) {
+  @primitiveTrait({
+    type: "boolean",
+    name: "Use authentication",
+    description:
+      "If true, members may inherit use of the configured authentication method when they do not set `useAuthentication` themselves."
+  })
+  useAuthentication?: boolean;
+}
